2cdb0572aa5bc8bbc19e2a1701fd20a42fd9525d
Сергей Трофимов <sm.trofimov2004@gmail.com>
2024-11-26 20:40:56 +0300

    /profile placeholder

--- src/cogs/members/__init__.py
+++ src/cogs/members/__init__.py
@@ -1,6 +1,8 @@
 from src.bot import JesterBot
 from ._on_guild_cog import OnGuildCog
+from ._profile_cog import ProfileCog
 
 
 def setup(bot: JesterBot) -> None:
+    bot.add_cog(ProfileCog(bot))
     bot.add_cog(OnGuildCog(bot))
---
+++ src/cogs/members/_profile_cog.py
@@ -0,0 +1,41 @@
+import disnake
+from disnake.ext import commands
+
+from src.bot import JesterBot
+from src.utils._convertes import bot_excluding
+from ._api_interaction import get_member
+from src.utils._embeds import BaseEmbed
+from src.localization import get_localizator
+
+
+_ = get_localizator("profile")
+
+
+class ProfileCog(commands.Cog):
+    def __init__(self, bot: JesterBot) -> None:
+        self.bot = bot
+
+    @commands.slash_command()
+    async def profile(
+        self,
+        interaction: disnake.GuildCommandInteraction,
+        member: disnake.Member=commands.Param(converter=bot_excluding, default=None) 
+    ) -> None:
+        await interaction.response.defer()
+
+        guild = interaction.guild
+        if not member:
+            member = interaction.author
+
+        member_data = await get_member(member.id, guild.id)
+        await interaction.followup.send(
+            embed = BaseEmbed(
+                title = _("profile_embed_title", username=member.display_name),
+                description = _(
+                    "profile_embed_desc",
+                    exp=member_data.experience,
+                    coins=member_data.coins,
+                    messages=member_data.message_count,
+                )
+            ).set_thumbnail(member.display_avatar.url)
+        )
