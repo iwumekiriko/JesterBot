1c785e9af66a0f9a827a16a701467ac9fc7d4e0d
Сергей Трофимов <sm.trofimov2004@gmail.com>
2025-04-11 00:14:27 +0300

    adding active_lootboxes to config

--- src/models/config/lootboxes_config.py
+++ src/models/config/lootboxes_config.py
@@ -8,12 +8,14 @@ from ..lootboxes import LootboxTypes
 class LootboxesConfig(BaseConfig):
     roles_lootbox_key_price: int | None = None
     backgrounds_lootbox_key_price: int | None = None
+    active_lootboxes: str | None = None
 
     def to_dict(self) -> dict:
         return {
             "guildId": self.guild_id,
             "rolesLootboxKeyPrice": self.roles_lootbox_key_price,
             "backgroundsLootboxKeyPrice": self.backgrounds_lootbox_key_price,
+            "activeLootboxes": self.active_lootboxes
         }
 
     def get_price(self, type: LootboxTypes) -> int:
