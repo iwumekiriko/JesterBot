806b2f0abbc4deeea430b4c206f76d6a716b2628
Сергей Трофимов <sm.trofimov2004@gmail.com>
2025-05-13 13:00:43 +0300

    added currency enum

---
+++ src/utils/enums/_currency_enum.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+from src.localization import get_localizator
+
+
+_ = get_localizator("enums")
+
+
+class Currency(str, Enum):
+    COINS = "coins"
+    CRYSTALS = "crystals"
+
+    @property
+    def translated(self) -> str:
+        return _({
+            Currency.COINS: "currency-coins_name",
+            Currency.CRYSTALS: "currency-crystals_name"
+        }[self])
+
+    def get_icon(self, guild_id: int) -> str:
+        from src.config import cfg
+        economy_cfg = cfg.economy_cfg(guild_id)
+
+        return {
+            Currency.COINS: economy_cfg.default_currency_icon,
+            Currency.CRYSTALS: economy_cfg.donate_currency_icon
+        }[self]
