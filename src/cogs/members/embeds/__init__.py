b62762b5159df201d1af154253f7f47a61b1fee1
Сергей Трофимов <sm.trofimov2004@gmail.com>
2025-05-21 01:44:20 +0300

    welcome message

--- src/cogs/members/embeds/__init__.py
+++ src/cogs/members/embeds/__init__.py
@@ -1,4 +1,5 @@
-from .nitro_boosting_embed import NitroBoostingEmbed
+from ._nitro_boosting_embed import NitroBoostingEmbed
+from ._welcome_embed import WelcomeEmbed
 
 
-__all__ = ('NitroBoostingEmbed', )
+__all__ = ('NitroBoostingEmbed', 'WelcomeEmbed', )
---
+++ src/cogs/members/embeds/_welcome_embed.py
@@ -0,0 +1,10 @@
+from src.utils.ui import BaseEmbed
+
+
+class WelcomeEmbed(BaseEmbed):
+    def __init__(self, **kwargs) -> None:
+        super().__init__(
+            title = kwargs.get('title'),
+            description = kwargs.get('description'),
+            color = 0xFFB59E
+        )
