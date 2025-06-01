9f12f8a61ff5f0c650f8ac75849792f423a9c7f0
Сергей Трофимов <sm.trofimov2004@gmail.com>
2025-06-01 16:25:22 +0300

    using own api for gifs instead of tenor

--- src/cogs/interactions/_api_interaction.py
+++ src/cogs/interactions/_api_interaction.py
@@ -1,29 +1,29 @@
+from typing import List
 import aiohttp
 import random
+import json
+
+from src.models.interactions import InteractionsAsset
 
 from ._interactions_choice import InteractionActions, InteractionTypes
-from src.settings import TENOR_API_KEY
 from src.utils._exceptions import CustomException
+from src.api_client import APIClient
 
 
 async def get_gif(
-    query: InteractionActions,
-    type: InteractionTypes,
-    limit: int = 30
+    guild_id: int,
+    action_value: int,
+    type_type: int
 ) -> str:
-    async with aiohttp.ClientSession() as session:
-        async with session.get(
-            'https://tenor.googleapis.com/v2/search?' 
-            f'q={type}%20{query}&'
-            f'key={TENOR_API_KEY}&'
-            'contentfilter=medium&'
-            'media_filter=minimal&'
-            f'limit={limit}'
-        ) as response:
-            if response.status == 200:
-                json_response = await response.json()
-                return json_response['results'][random.randint(0, limit-1)]['media_formats']['gif']['url']
-            else:
-                error_message = await response.text()
-                raise CustomException("Tenor API is not responding. "
-                                    f"Status code: {response.status}. Error: {error_message}")
+    endpoint = f"Interactions/{guild_id}/{action_value}/{type_type}"
+    async with APIClient() as client:
+        return (await client.get(endpoint))['assetUrl']
+
+
+async def upload_gifs(guild_id: int, gifs: List[InteractionsAsset]) -> None:
+    endpoint = f"Interactions/{guild_id}/upload"
+    async with APIClient() as client:
+        await client.put(
+            endpoint,
+            body=[gif.to_dict() for gif in gifs] # type: ignore
+        )
--- src/cogs/interactions/_interact_cog.py
+++ src/cogs/interactions/_interact_cog.py
@@ -1,15 +1,28 @@
+from typing import List
+import pandas as pd
+from io import BytesIO
+
 import disnake
 from disnake.ext import commands
 
 from src.bot import JesterBot
+from src.localization import get_localizator
+from src.logger import get_logger
+
 from src.utils._converters import inter_member
 from ._interactions_choice import InteractionActions, InteractionTypes
-from src.localization import get_localizator
+from src.models.interactions import InteractionsAsset
 from src.utils.ui import BaseEmbed
-from ._api_interaction import get_gif
+from ._api_interaction import get_gif, upload_gifs
 
 
+logger = get_logger()
 _ = get_localizator("interactions.common")
+
+
+ACCEPTABLE_DF_COLUMNS = ['Url', 'Action', 'Type']
+ACCEPTABLE_ACTIONS = [action.name.lower() for action in InteractionActions]
+ACCEPTABLE_TYPES = [type.name.lower() for type in InteractionTypes]
 
 
 limits = {
@@ -31,18 +44,78 @@ class UserInteractionsCog(commands.Cog):
         member: disnake.Member = commands.Param(
             converter=inter_member, description=_("user_interaction_member_param")),
         action = commands.Param(
-            choices={choice.translated_name: choice.name.lower() for choice in InteractionActions},
+            choices={choice.translated_name: str(choice.value) for choice in InteractionActions},
             description=_("user_interaction_action_param")),
         type = commands.Param(
-            choices={type.translated_name: type.name.lower() for type in InteractionTypes},
+            choices={type.translated_name: str(type.value) for type in InteractionTypes},
             description=_("user_interaction_type_param"))
     ) -> None:
+        action = InteractionActions(int(action))
+        type = InteractionTypes(int(type))
+
         await interaction.response.send_message(
             content=member.mention,
-            embed=BaseEmbed(
-                description=_(action,
+            embed=BaseEmbed(    
+                description=_(action.name.lower(),
                             command_user=interaction.user.id,
                             command_target=member.id)
-            ).set_image(await get_gif(action, type, limit=limits[action])))
+            ).set_image(await get_gif(
+                interaction.guild.id,
+                action.value,
+                type.value
+            )))
 
+    @commands.slash_command(description=_("interactions-gifs-upload_desc"))
+    async def _ug(
+        self,
+        interaction: disnake.GuildCommandInteraction,
+        attachment: disnake.Attachment = commands.Param(
+            description=_("interactions-gifs-upload_attachment_param"))
+    ) -> None:
+        if (attachment.filename and not 
+            ("csv" in attachment.filename or "xlsx" in attachment.filename)
+        ):
+            await interaction.response.send_message("wrong file type")
+            return
 
+        await interaction.response.defer(ephemeral=True)
+        extension = attachment.filename.split('.')[-1]
+
+        match (extension):
+            case "csv":
+                df = pd.read_csv(BytesIO(await attachment.read()))
+            case "xlsx":
+                df = pd.read_excel(BytesIO(await attachment.read()))
+
+        if not self.is_dataframe_correct(df):
+            await interaction.followup.send("not correct dataframe structure")
+
+        gifs = await self.handle_dataframe(df)
+        await upload_gifs(interaction.guild.id, gifs)
+        await interaction.followup.send("in process...", ephemeral=True)
+
+    async def handle_dataframe(self, df: pd.DataFrame) -> List[InteractionsAsset]:
+        if not (df.columns.to_list() == ['Url', 'Action', 'Type']):
+            return []
+
+        cleaned_df = df.dropna()
+        interactions_assets: List[InteractionsAsset] = []
+        for _, row in cleaned_df.iterrows():
+            if not (i_action := str(row['Action']).lower()) in ACCEPTABLE_ACTIONS:
+                continue
+
+            if not (i_type := str(row['Type']).lower()) in ACCEPTABLE_TYPES:
+                continue
+
+            url = row['Url']
+            action = InteractionActions[i_action.upper()] 
+            type = InteractionTypes[i_type.upper()]
+            interactions_assets.append(InteractionsAsset(url, action, type))
+
+        return interactions_assets
+
+    def is_dataframe_correct(self, df: pd.DataFrame) -> bool:
+        if not (df.columns.to_list() == ACCEPTABLE_DF_COLUMNS):
+            return False
+        
+        return True
