57598d54378e3e2799611741a1bb452c60e31d89
Сергей Трофимов <sm.trofimov2004@gmail.com>
2026-05-13 17:01:25 +0300

    add save files to channel func

--- src/bot.py
+++ src/bot.py
@@ -47,6 +47,33 @@ class JesterBot(commands.Bot):
         await self._sync_voice_users()
         print(f"[{datetime.now().strftime('%c')}]: {self.user}'s ready!")
 
+    async def save_avatar(self, user: disnake.Member | disnake.User) -> str:
+        url = None
+        if isinstance(user, disnake.Member):
+            url = await self.save_file(user.guild.id, await user.display_avatar.to_file())
+        return url or user.display_avatar.url
+
+    async def save_file(self, guild_id: int, file: disnake.File) -> Optional[str]:
+        from src.config import cfg
+        channels_cfg = cfg.channels_cfg(guild_id)
+
+        if not (image_saver_ch_id := channels_cfg.image_saver_channel_id):
+            logger.warning("image saver channel is not configured!")
+            return
+
+        image_saver_ch = self.get_channel(image_saver_ch_id)
+        if not isinstance(image_saver_ch, disnake.TextChannel):
+            logger.warning("image saver channel [%s] is not text channel", image_saver_ch)
+            return
+
+        try:
+            file.filename = f"SPOILER_{file.filename}"
+            message = await image_saver_ch.send(file=file)
+        except (disnake.HTTPException, disnake.Forbidden):
+            return None
+
+        return message.attachments[0].url
+
     async def _sync_voice_users(self) -> None:
         if not settings.API_REQUIRED:
             return
--- src/models/config/channels_config.py
+++ src/models/config/channels_config.py
@@ -8,11 +8,13 @@ class ChannelsConfig(BaseConfig):
     general_channel_id: int | None = None
     offtop_channel_id: int | None = None
     nitro_boosting_channel_id: int | None = None
+    image_saver_channel_id: int | None = None
 
     def to_dict(self) -> dict:
         return {
             "guildId": self.guild_id,
             "generalChannelId": self.general_channel_id,
             "offtopChannelId": self.offtop_channel_id,
-            "nitroBoostingChannelId": self.nitro_boosting_channel_id
+            "nitroBoostingChannelId": self.nitro_boosting_channel_id,
+            "imageSaverChannelId": self.image_saver_channel_id
         }
