364469e09be33be061552fc9309f901ba9ed8690
Сергей Трофимов <sm.trofimov2004@gmail.com>
2026-05-14 14:26:37 +0300

    update modals to components_v2

--- src/utils/ui/__init__.py
+++ src/utils/ui/__init__.py
@@ -1,5 +1,5 @@
 from ._embeds import BaseEmbed, ExceptionEmbed, SuccessEmbed, WarningEmbed
-from ._modals import BaseModal, ModalTextInput
+from ._modals import BaseModal, ModalTextInput, ModalLabel, ModalSelectMenu, ModalTextDisplay
 from ._paginators import Paginator
 from ._views import BaseView, State
 from ._switchers import ViewSwitcher
@@ -7,7 +7,7 @@ from ._switchers import ViewSwitcher
 
 __all__ = (
     'BaseEmbed', 'ExceptionEmbed', 'SuccessEmbed', 'WarningEmbed',
-    'BaseModal', 'ModalTextInput',
+    'BaseModal', 'ModalTextInput', 'ModalLabel', 'ModalTextDisplay', 'ModalSelectMenu',
     'Paginator',
     'BaseView', 'State',
     'ViewSwitcher'
